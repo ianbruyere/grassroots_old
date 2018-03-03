206d8d67a22757fc1e7ae47abc9d8744a6ced42d
Ian Bruyere <loudthoughts888@gmail.com>
2018-03-02 20:21:21 -0800

    whoopsy forgot you small child

--- grassroots/frontend/src/services/api.js
+++ grassroots/frontend/src/services/api.js
@@ -1,12 +1,7 @@
 import axios from 'axios'
 
-<<<<<<< HEAD
-export function fetchCongressData() {
-    return axios.get("http://127.0.0.1:8000/api/congressmembers/")  
-=======
 
 export function fetchSenatorData() {
     return axios.get("http://127.0.0.1:8000/api/congressmembers/")
     .then(res => res.data)
->>>>>>> experiment
 }
