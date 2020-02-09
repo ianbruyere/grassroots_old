f31ab16ae045b5c743875e534338ef42dec1b181
Ian Bruyere <6414971+ianbruyere@users.noreply.github.com>
2020-02-09 09:23:33 -0500

    reconfiguring old bitbucket repo

--- grassroots/grassroots/wsgi.py
+++ grassroots/grassroots/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grassroots.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grassroots.development_settings")
 
 application = get_wsgi_application()
